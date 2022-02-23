e0b3957fe42453035db79e9edccb71cc413e68a2
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2022-02-23 17:05:20 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -7,4 +7,15 @@
 TEST_CASE("infers the breach according to limits") {
   TemperatureAlert obj;
   REQUIRE(obj.inferBreach(12, std::make_pair(20, 30)) == TOO_LOW);
+  REQUIRE(obj.inferBreach(40, std::make_pair(20, 30)) == TOO_HIGH);
+  REQUIRE(obj.inferBreach(25, std::make_pair(20, 30)) == NORMAL);
+}
+
+TEST_CASE("checks initialization") {
+  TemperatureAlert obj;
+  TempBoundary tempBoundary;
+  CHECK(getValuefromKey(obj._tempLimitMap, PASSIVE_COOLING, &tempBoundary));
+  CHECK(getValuefromKey(obj._tempLimitMap, HI_ACTIVE_COOLING, &tempBoundary));
+  CHECK(getValuefromKey(obj._tempLimitMap, MED_ACTIVE_COOLING, &tempBoundary));
+  CHECK_FALSE(getValuefromKey(obj._tempLimitMap, 4, &tempBoundary));
 }
