c5d5d45d0ae64b86b5d47e353a463e9e9ac4729a
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2022-02-23 10:06:59 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -4,22 +4,28 @@
 
 #include "typewise-alertTypes.h"
 
-/*Member functions*/
-
-void initializeTemperatureLimits();
-void initializeAlertType();
-
-BreachType inferBreach(double value, TempBoundary tempBoundary);
-void sendAlert(BreachType breachType, AlertTarget alertTarget);
-
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
-
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
-
-/*Member variables*/
-TempLimitMap _tempLimitMap;
-AlertTargetMap _alertTargetMap;
+class TemperatureAlert
+{
+public:
+   TemperatureAlert();
+   ~TemperatureAlert() {}
+   /*Member functions*/
+   
+   void initializeTemperatureLimits();
+   void initializeAlertType();
+   
+   BreachType inferBreach(double value, TempBoundary tempBoundary);
+   void sendAlert(BreachType breachType, AlertTarget alertTarget);
+   
+   void checkAndAlert(
+     AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+   
+   void sendToController(BreachType breachType);
+   void sendToEmail(BreachType breachType);
+   
+   /*Member variables*/
+   TempLimitMap _tempLimitMap;
+   AlertTargetMap _alertTargetMap;
+};
 
 #endif
